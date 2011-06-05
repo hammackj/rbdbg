9647045fe0b10c659ed7f06bcc306dbe91c80bd8
Jacob Hammack <jacob.hammack@hammackj.com>
2011-06-05 12:27:23 -0500

    Added new files

--- lib/rbdbg/debugger/osx.rb
+++ lib/rbdbg/debugger/osx.rb
@@ -5,7 +5,12 @@ module RbDbg
 	end
 end
 
-require 'rbdbg/debugger/osx/osx_debugger'
+
 require 'rbdbg/debugger/osx/mach'
 require 'rbdbg/debugger/osx/ptrace'
 require 'rbdbg/debugger/osx/threadstate64'
+require 'rbdbg/debugger/osx/proc'
+require 'rbdbg/debugger/osx/externproc'
+require 'rbdbg/debugger/osx/sysctl'
+
+require 'rbdbg/debugger/osx/osx_debugger'
