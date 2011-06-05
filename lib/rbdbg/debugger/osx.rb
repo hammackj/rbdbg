ca81febe99029035b1e523e920669b0a5147981a
Jacob Hammack <jacob.hammack@hammackj.com>
2011-06-05 13:05:41 -0500

    Added the machine header

--- lib/rbdbg/debugger/osx.rb
+++ lib/rbdbg/debugger/osx.rb
@@ -12,5 +12,6 @@ require 'rbdbg/debugger/osx/threadstate64'
 require 'rbdbg/debugger/osx/proc'
 require 'rbdbg/debugger/osx/externproc'
 require 'rbdbg/debugger/osx/sysctl'
+require 'rbdbg/debugger/osx/machine'
 
 require 'rbdbg/debugger/osx/osx_debugger'
